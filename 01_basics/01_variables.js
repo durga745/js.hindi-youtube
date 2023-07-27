7363a6291285627908708da8dc288ce7dd50b0f5
durga745 <72251345+durga745@users.noreply.github.com>
2023-07-27 19:13:18 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "durga745@gmail.com";
+var accountPassword = "12345";
+accountCity = "Rourkela";
+let accountState;
+
+// accountId = 2; // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodeJS, not browser
+
+console.log(3 + 3) 
+
+console.log("Durga")
+
+
+let name = "Durga"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Durga")
+console.log(typeof age);
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined
