79f6ee346651d86d1a51dcd2c17e2f60ab509f5e
durga745 <72251345+durga745@users.noreply.github.com>
2023-07-28 12:16:57 +0000

    hey

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -17,8 +17,17 @@ const hundreds = 1000000
 
 // ++++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.4));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.8));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+// console.log(Math.random());
+// console.log(Math.round((Math.random() * 10) + 1));
+
+const min = 1
+const max = 9
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
